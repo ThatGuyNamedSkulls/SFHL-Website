5d26df7359612a37de63c02260195d02ac7eb602
Skulls <diogo.charrua07@gmail.com>
2026-09-16 12:56:46 +0100

    Update to whole website and Bot. Added substitute stuff

--- lib/match-mode.ts
+++ lib/match-mode.ts
@@ -1,5 +1,5 @@
 /** Live queue format. Website lobby size follows this (bot `/gamemode` can still switch). */
-export const MATCH_TEAM_SIZE = 5;
+export const MATCH_TEAM_SIZE = 1;
 export const MATCH_QUEUE_SIZE = MATCH_TEAM_SIZE * 2;
 export const MATCH_MODE_LABEL = `${MATCH_TEAM_SIZE}v${MATCH_TEAM_SIZE}`;
 export const PARTY_MAX_SIZE = 5;
